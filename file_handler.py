b41e63aa293d4e30fbb0d8b3d704b13b14d30386
mdevelopment9 <mason.yerrid@gmail.com>
2023-12-06 11:14:21 -0500

    delete temp audio file before trying to make new file

--- file_handler.py
+++ file_handler.py
@@ -1,12 +1,14 @@
 from pydub import AudioSegment
 from scipy.io import wavfile
+import os.path
 import scipy.io
 """convert file from supported format and convert to wav"""
 class FileHandler:
     wavfile_loc = "tmp.wav"
 
     def __init__(self, input_file: str):
-
+        if os.path.isfile(self.wavfile_loc):
+            os.remove(self.wavfile_loc)
         split_input = input_file.split(".")
         file_end = split_input[len(split_input)-1]
         sound_file = AudioSegment.from_file(input_file,format=file_end)
@@ -16,5 +18,5 @@ class FileHandler:
     def getFileData(self):
         samplerate,data = wavfile.read(self.wavfile_loc)
         length = data.shape[0] / samplerate
-        #returns the sample rate, the audio channels, and the length of the audio file
+        # returns the sample rate, the audio channels, and the length of the audio file
         return samplerate,data.shape[len(data.shape) - 1],length
